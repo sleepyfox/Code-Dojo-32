3c78ea5ce0a9a48953630db693af767a74b0c18f
Sleepy Fox <sleepyfox@gmail.com>
2015-02-02 16:01:13 +0000

    added test for print-diamond 'B' and a naive implementation

--- Clojure/diamond/test/diamond/core_test.clj
+++ Clojure/diamond/test/diamond/core_test.clj
@@ -2,8 +2,19 @@
   (:require [clojure.test :refer :all]
             [diamond.core :refer :all]))
 
-(defn print-diamond [a] a)
+(defn print-diamond [a]
+  (if (= a "A") ["A"]
+    (if (= a "B") [" A ", "BBB", " A "] 0)))
 
 (deftest a-test
   (testing "print-diamond 'A' returns 'A'"
-    (is (= (print-diamond 'A') 'A'))))
+    (is (= (print-diamond "A") ["A"])))
+  (testing "print-diamond 'B' returns a list of three strings"
+    (is (= (count (print-diamond "B")) 3)))
+  (testing "print-diamond 'B' first string is ' A '"
+    (is (= (first (print-diamond "B")) " A ")))
+  (testing "print-diamond 'B' middle string is 'BBB'"
+    (is (= (nth (print-diamond "B") 1) "BBB")))
+  (testing "print-diamond 'B' last string is ' A '"
+    (is (= (nth (print-diamond "B") 2) " A ")))
+)
