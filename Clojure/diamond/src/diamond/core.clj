00edb312d6837f23928ed5a28f0b2a3343c305e4
Sleepy Fox <sleepyfox@gmail.com>
2015-02-04 12:52:36 +0000

    moved implementation out of test file into src/diamond/core.clj

--- Clojure/diamond/src/diamond/core.clj
+++ Clojure/diamond/src/diamond/core.clj
@@ -1,6 +1,62 @@
 (ns diamond.core
   (:gen-class))
 
+(def space (char 32))
+
+(defn n-spaces [n]
+  (if (<= n 0) "" 
+    (apply str (repeat n space))))
+
+(defn previous-char [c] 
+  (if (= c \A) nil
+    (char (- (int c) 1))))
+
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
+
+; Calculations for edge and middle spaces
+; 0 ..A..  2,-1 (w-1/2)-R, -1+2R
+; 1 .B.B.  1,1 (w-1/2)-R, -1+2R
+; 2 C...C  0,3 (w-1/2)-R, -1+2R
+;
+; 0 ...A...  3,-1 (w-1/2)-R, -1+2R
+; 1 ..B.B..  2,1  (w-1/2)-R, -1+2R
+; 2 .C...C.  1,3  (w-1/2)-R, -1+2R
+; 3 D.....D  0,5  (w-1/2)-R, -1+2R
+; 
+; width = (letter-65)*2+1
+
+(defn make-row [width letter]
+  (def row (- (int letter) 65))
+  (def edge-spaces (- (/ (- width 1) 2) row))
+  (def middle-spaces (+ -1 (* 2 row)))
+  (let [s (n-spaces edge-spaces) m (n-spaces middle-spaces)]
+    (if (= letter \A)
+      (str s letter s)
+      (str s letter m letter s))))
+
+(defn make-rows [letter]
+  (def width (inc (* (- (int letter) 65) 2)))
+  (def new-row (partial make-row width))
+  (map new-row (char-range \A letter)))
+
+(defn first-part [c] 
+  (def steps (- (int c) 65))
+  (if (= steps 0) nil 
+    (if (= steps 1) [" A "] ["  A  " " B  B "])))
+
+(defn last-part [c] 
+  (first-part c))
+
+(defn middle-row [c] 
+  (def square-width (+ 1 (* 2 (- (int c) 65))))
+  (def middle-row-spaces (n-spaces (- square-width 2)))
+  (str c middle-row-spaces c))
+
+(defn print-diamond [c]
+  (if (= c \A) ["A"]
+    (flatten [(first-part c) (middle-row c) (last-part c)])))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
