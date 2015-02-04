160d9b906ae641c85c45e00c16f216b92eba77ce
Sleepy Fox <sleepyfox@gmail.com>
2015-02-04 12:45:17 +0000

    added new tests and implementation for a different approach to making rows

--- Clojure/diamond/test/diamond/core_test.clj
+++ Clojure/diamond/test/diamond/core_test.clj
@@ -5,16 +5,46 @@
 (def space (char 32))
 
 (defn n-spaces [n]
-  (if (= n 0) "" 
+  (if (<= n 0) "" 
     (apply str (repeat n space))))
 
 (defn previous-char [c] 
   (if (= c \A) nil
     (char (- (int c) 1))))
 
+(defn char-range [start end]
+  (map char (range (int start) (inc (int end)))))
+
+; Calculations for edge and middle spaces
+; 0 ..A..  2,-1 (w-1/2)-R, -1+2R
+; 1 .B.B.  1,1 (w-1/2)-R, -1+2R
+; 2 C...C  0,3 (w-1/2)-R, -1+2R
+;
+; 0 ...A...  3,-1 (w-1/2)-R, -1+2R
+; 1 ..B.B..  2,1  (w-1/2)-R, -1+2R
+; 2 .C...C.  1,3  (w-1/2)-R, -1+2R
+; 3 D.....D  0,5  (w-1/2)-R, -1+2R
+; 
+; width = (letter-65)*2+1
+
+(defn make-row [width letter]
+  (def row (- (int letter) 65))
+  (def edge-spaces (- (/ (- width 1) 2) row))
+  (def middle-spaces (+ -1 (* 2 row)))
+  (let [s (n-spaces edge-spaces) m (n-spaces middle-spaces)]
+    (if (= letter \A)
+      (str s letter s)
+      (str s letter m letter s))))
+
+(defn make-rows [letter]
+  (def width (inc (* (- (int letter) 65) 2)))
+  (def new-row (partial make-row width))
+  (map new-row (char-range \A letter)))
+
 (defn first-part [c] 
-  (if (= c \A) nil 
-    [(str " " (str (previous-char c)) " ")]))
+  (def steps (- (int c) 65))
+  (if (= steps 0) nil 
+    (if (= steps 1) [" A "] ["  A  " " B  B "])))
 
 (defn last-part [c] 
   (first-part c))
@@ -31,6 +61,8 @@
 ; Tests here
 ;
 (deftest n-spaces-test
+  (testing "n-spaces -1 should return the empty string"
+    (is (= (n-spaces -1) "")))
   (testing "n-spaces 0 should return the empty string"
     (is (= (n-spaces 0) "")))
   (testing "n-spaces 1 should return ' '"
@@ -44,13 +76,21 @@
   (testing "previous-char 'B' is 'A'"
     (is (= (previous-char \B) \A))))
 
+(deftest char-range-test
+  (testing "char-range A should be (A)"
+    (is (= (char-range \A \A) (list \A))))
+  (testing "char-range A E should be (A B C D E)"
+    (is (= (char-range \A \E) (list \A \B \C \D \E)))))
+
 (deftest first-part-test 
   (testing "first-part 'A' is nil"
     (is (= (first-part \A) nil)))
   (testing "first-part 'B' is a vector of length 1"
     (is (= (count (first-part \B)) 1)))
   (testing "first-part 'B' is [' A ']"
-    (is (= (first (first-part \B)) " A "))))
+    (is (= (first (first-part \B)) " A ")))
+  (testing "first-part 'C' is a vector of length 2"
+    (is (= (count (first-part \C)) 2))))
 
 (deftest diamond-test-A
   (testing "print-diamond 'A' returns 'A'"
@@ -65,3 +105,17 @@
     (is (= (nth (print-diamond \B) 1) "B B")))
   (testing "print-diamond 'B' last string is ' A '"
     (is (= (nth (print-diamond \B) 2) " A "))))
+
+(deftest make-row-test
+  (testing "make-row 1 A should return 'A'"
+    (is (= (make-row 1 \A) "A")))
+  (testing "make-row 3 A should return ' A '"
+    (is (= (make-row 3 \A) " A ")))
+  (testing "make-row 5 A should return '  A  '"
+    (is (= (make-row 5 \A) "  A  ")))
+  (testing "make-row 3 B should return 'B B'"
+    (is (= (make-row 3 \B) "B B")))
+  (testing "make-row 5 B should return ' B B '"
+    (is (= (make-row 5 \B) " B B ")))
+  (testing "make-row 5 C should return 'C   C'"
+    (is (= (make-row 5 \C) "C   C"))))
