a62bb55281eb8e3c99bccd8ae176b1217b2dbbd2
@sleepyfox <sleepyfox@gmail.com>
2021-07-05 11:36:33 +0100

    Fixed doco

--- Clojure/diamond/project.clj
+++ Clojure/diamond/project.clj
@@ -1,8 +1,8 @@
 (defproject diamond "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "An example implementation of the Diamond code kata"
+  :url "https://github.com/sleepyfox/Code-Dojo-32"
+  :license {:name "CC-BY-SA-NC 3.0"
+            :url "http://creativecommons.org/licenses/by-nc-sa/3.0/"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot diamond.core
   :target-path "target/%s"
