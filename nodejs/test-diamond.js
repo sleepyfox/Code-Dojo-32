a13ef9988667b3b5b33e840c4a2bd184870bdee0
@sleepyfox <sleepyfox@gmail.com>
2021-07-13 11:44:56 +0100

    First real acceptance test

--- nodejs/test-diamond.js
+++ nodejs/test-diamond.js
@@ -1,5 +1,13 @@
-describe("A suite", () => {
-  it("contains spec with an expectation", () => {
-    expect(true).toBe(true)
+class Diamond {
+  to_str() {
+    return(' A \nB B\n A \n')
+  }
+}
+
+// TODO: wire acceptance test to real implementation
+describe('A diamond based on B', () => {
+  it('should look like', () => {
+    let diamond = new Diamond('B')
+    expect(diamond.to_str()).toEqual(' A \nB B\n A \n')
   })
 })
